83471a004a3053ad33ba91e690f8dc30e9adf28a
Lais <lais.salesms@gmail.com>
2023-05-17 17:10:35 -0300

    Link de volta para Home em Login.tsx

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom"
+
 function Login() {
     return (
         <>
+            <Link to="/home" className='link'>
+                <h1>Home</h1>
+            </Link>
             <h1>Login</h1>
         </>
     )
