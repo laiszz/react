20538e03734f679cec9c29e2add5d9640738c430
Lais <lais.salesms@gmail.com>
2023-05-22 09:37:11 -0300

    Listar postagens

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './paginas/home/Home'
 import Login from './paginas/login/Login'
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario'
+import ListaTema from './components/temas/listatema/ListaTema'
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem'
 import './App.css'
 
 function App() {
@@ -20,6 +22,8 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/postagens" element={<ListaPostagem />} />
         </Routes>
 
         <Footer />
